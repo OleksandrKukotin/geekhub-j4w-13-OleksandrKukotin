28b0456438c8610c252b8e1724833d3828d4794d
kirn0al <oleksandr.kukotin@gmail.com>
2023-10-18 13:40:05 +0300

    L01 Configured IDE+GitHub and added greeting command to the main method of ApplicationStarter class

--- Homework/java-core/01-Introduction/src/main/java/org/geekhub/ApplicationStarter.java
+++ Homework/java-core/01-Introduction/src/main/java/org/geekhub/ApplicationStarter.java
@@ -2,5 +2,7 @@ package org.geekhub;
 
 public class ApplicationStarter {
 
-    // TODO write code here AND REMOVE THIS MESSAGE
+    public static void main(String[] args) {
+        System.out.println("Nice to see you, dear mentor! Thanks for the work you're doing for me and my classmates!");
+    }
 }
