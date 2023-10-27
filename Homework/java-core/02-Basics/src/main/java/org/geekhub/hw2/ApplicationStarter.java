70fcb0e5d33dedd5429af43dfb654f3a2c9e1fdd
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2023-10-27 22:50:41 +0300

    L02 Implemented displaying average grades by students and by subjects, small changes in the collecting data function

--- Homework/java-core/02-Basics/src/main/java/org/geekhub/hw2/ApplicationStarter.java
+++ Homework/java-core/02-Basics/src/main/java/org/geekhub/hw2/ApplicationStarter.java
@@ -8,20 +8,34 @@ public class ApplicationStarter {
     private static final int NUMBER_OF_SUBJECTS = SUBJECTS.length;
     public static final int DEFAULT_GRADE = -1;
     public static final int NAME_INDEX = 0;
+    private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Error: The application have no arguments");
-            System.exit(1);
+    private static void printAveragesByStudents(String[][] data) {
+        System.out.println("Average grades for Each Student: ");
+        for (int i = 0; i < data.length; i++) {
+            float average = 0;
+            System.out.printf(data[i][NAME_INDEX] + ": ");
+            for (int j = 1; j < data[i].length; j++) {
+                average += Float.parseFloat(data[i][j]);
+            }
+            System.out.println(average / NUMBER_OF_SUBJECTS);
         }
-        final Scanner scanner = new Scanner(System.in);
-        String[][] studentsData = collectStudentsData(Integer.parseInt(args[0]), scanner);
-        printAveragesByStudents(studentsData);
-        printAveragesBySubjects(studentsData);
-        scanner.close();
+        System.out.println();
+    }
+    private static void printAveragesBySubjects(String[][] data) {
+        System.out.println("Average grades For Each Subject: ");
+        for (int i = 1; i <= NUMBER_OF_SUBJECTS; i++) {
+            System.out.printf(SUBJECTS[i-1] + ": ");
+            float average = 0;
+            for (int j = 0; j < data.length; j++) {
+                average += Float.parseFloat(data[j][i]);
+            }
+            System.out.println(average / data.length);
+        }
+        System.out.println();
     }
 
-    private static String[][] collectStudentsData(int numberOfStudents, Scanner scanner) {
+    private static String[][] collectStudentsData(int numberOfStudents) {
         String[][] students = new String[numberOfStudents][NUMBER_OF_SUBJECTS + 1];
         for (int i = 0; i < numberOfStudents; i++) {
             String studentName = "";
@@ -31,54 +45,26 @@ public class ApplicationStarter {
                 studentName = scanner.nextLine();
             }
             students[i][NAME_INDEX] = studentName;
-            collectStudentGrades(students[i], scanner);
+            for (int j = 1; j <= NUMBER_OF_SUBJECTS; j++) {
+                float grade = DEFAULT_GRADE;
+                while(grade < 0 || grade > 100) {
+                    System.out.printf(SUBJECTS[j - 1] + " Grade (0-100): ");
+                    grade = scanner.nextFloat();
+                }
+                students[i][j] = String.valueOf(grade);
+            }
             System.out.println();
         }
         return students;
     }
 
-    private static void collectStudentGrades(String[] studentData, Scanner scanner) {
-        for (int i = 1; i <= NUMBER_OF_SUBJECTS; i++) {
-            float grade = DEFAULT_GRADE;
-            while (grade < 0 || grade > 100) {
-                System.out.printf(SUBJECTS[i - 1] + " Grade (0-100): ");
-                grade = scanner.nextFloat();
-            }
-            studentData[i] = String.valueOf(grade);
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Error: The application have no arguments");
+            System.exit(1);
         }
-    }
-
-    private static void printAveragesByStudents(String[][] data) {
-        System.out.println("Average grades for Each Student: ");
-        for (int i = 0; i < data.length; i++) {
-            System.out.printf(data[i][NAME_INDEX] + ": ");
-            System.out.println(calculateAverageForStudent(data[i]));
-        }
-        System.out.println();
-    }
-
-    private static float calculateAverageForStudent(String[] studentData) {
-        float average = 0;
-        for (int i = 1; i < studentData.length; i++) {
-            average += Float.parseFloat(studentData[i]);
-        }
-        return average / NUMBER_OF_SUBJECTS;
-    }
-
-    private static void printAveragesBySubjects(String[][] data) {
-        System.out.println("Average grades For Each Subject: ");
-        for (int i = 1; i <= NUMBER_OF_SUBJECTS; i++) {
-            System.out.printf(SUBJECTS[i - 1] + ": ");
-            System.out.println(calculateAverageForSubject(data, i));
-        }
-        System.out.println();
-    }
-
-    private static float calculateAverageForSubject(String[][] data, int subjectIndex) {
-        float average = 0;
-        for (int i = 0; i < data.length; i++) {
-            average += Float.parseFloat(data[i][subjectIndex]);
-        }
-        return average / data.length;
+        String[][] studentsData = collectStudentsData(Integer.parseInt(args[0]));
+        printAveragesByStudents(studentsData);
+        printAveragesBySubjects(studentsData);
     }
 }
