c7086c594e8b3d40df1934e3d2c57736205f555b
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2023-11-04 19:28:14 +0200

    L03 Fetched changes in OrkoStatTest from template

--- Homework/java-core/03-OOP/src/test/java/org/geekhub/hw3/orkostat/OrkoStatTest.java
+++ Homework/java-core/03-OOP/src/test/java/org/geekhub/hw3/orkostat/OrkoStatTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class OrkoStatTest {
+class OrkoStatTest {
 
     private OrkoStat orkoStat;
 
