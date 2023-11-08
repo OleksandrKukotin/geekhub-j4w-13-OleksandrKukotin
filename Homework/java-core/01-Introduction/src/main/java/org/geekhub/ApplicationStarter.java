96a0d879f08a867bc98fe2efe02927e37ca6cde4
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2023-11-08 16:44:34 +0200

    L01 Deleted Sout to avoid errors on GitHub Pipeline

--- Homework/java-core/01-Introduction/src/main/java/org/geekhub/ApplicationStarter.java
+++ Homework/java-core/01-Introduction/src/main/java/org/geekhub/ApplicationStarter.java
@@ -3,6 +3,6 @@ package org.geekhub;
 public class ApplicationStarter {
 
     public static void main(String[] args) {
-        System.out.println("Nice to see you, dear mentor! Thanks for the work you're doing for me and my classmates!");
+        System.exit(0);
     }
 }
