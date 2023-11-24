ff3980df30284645c4ca335a851a874cf020c9a2
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2023-11-24 22:20:57 +0200

    L06 Implemented duplicates check mechanism and upgraded arguments handling

--- Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/CatFactsApplication.java
+++ Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/CatFactsApplication.java
@@ -3,7 +3,6 @@ package org.geekhub.hw6;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class CatFactsApplication {
@@ -13,8 +12,8 @@ public class CatFactsApplication {
 
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("The app hasn't received any arguments! Try to add one to program arguments!");
-            System.exit(1);
+            String exceptionMessage = "The app hasn't received any arguments! Try to add one to program arguments!";
+            throw new IllegalArgumentException(exceptionMessage);
         }
         HttpClient httpClient = HttpClientBuilder.create().build();
         CatFactsApiService apiService = new CatFactsApiService(httpClient);
--- Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/FileService.java
+++ Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/FileService.java
@@ -7,37 +7,58 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public class FileService {
 
-    private final Path fileName;
+    private final Path filePath;
 
-    public FileService(Path fileName) {
-        this.fileName = fileName;
+    public FileService(Path filePath) {
+        this.filePath = filePath;
     }
 
     public void writeToFile(byte[] data) {
         if (createFile()) {
+            if (isDuplication(data)) {
+                System.out.println("It's duplicate :/");
+                return;
+            }
             try {
                 byte[] dataWithNewLine = new String(data, StandardCharsets.UTF_8).concat("\n").getBytes();
-                Files.write(this.fileName, dataWithNewLine, StandardOpenOption.APPEND);
+                Files.write(this.filePath, dataWithNewLine, StandardOpenOption.APPEND);
             } catch (IOException e) {
-                e.printStackTrace();
                 throw new WriteToFileException("I/O error occurred during writing data to file", e);
             }
+        } else {
+            throw new WriteToFileException("I/O error occurred during creating file", new IOException());
         }
     }
 
     private boolean createFile() {
-        if (Files.exists(this.fileName)) {
+        if (Files.exists(filePath)) {
             return true;
         } else {
             try {
-                Files.createFile(this.fileName);
+                Files.createFile(filePath);
                 return true;
             } catch (IOException e) {
                 return false;
             }
+        }
+    }
+
+    private boolean isDuplication(byte[] data) {
+        try {
+            List<String> readFacts = Files.readAllLines(filePath);
+            String dataToCheck = new String(data, StandardCharsets.UTF_8);
+            for (String line : readFacts) {
+                if (line.contains(dataToCheck)) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (IOException e) {
+            return false;
         }
     }
 }
