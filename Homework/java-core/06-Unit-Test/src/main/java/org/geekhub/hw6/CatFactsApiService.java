0ce7e8e955beb7715b700ddd94ca5799b102b390
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2023-11-25 19:24:13 +0200

    L06 Fixed third-request problem

--- Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/CatFactsApiService.java
+++ Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/CatFactsApiService.java
@@ -3,8 +3,10 @@ package org.geekhub.hw6;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.geekhub.hw6.exception.ApiExecutionException;
 
 import java.io.IOException;
@@ -14,9 +16,9 @@ import java.net.URISyntaxException;
 public class CatFactsApiService {
 
     private static final String CAT_FACT_API_URL = "https://catfact.ninja/fact";
-    private final HttpClient httpClient;
+    private final CloseableHttpClient httpClient;
 
-    public CatFactsApiService(HttpClient httpClient) {
+    public CatFactsApiService(CloseableHttpClient httpClient) {
         this.httpClient = httpClient;
     }
 
@@ -24,8 +26,9 @@ public class CatFactsApiService {
         try {
             URI uri = new URI(CAT_FACT_API_URL);
             HttpUriRequest request = new HttpGet(uri);
-            HttpResponse response = httpClient.execute(request);
-            return response.getEntity().getContent().readAllBytes();
+            try (CloseableHttpResponse response = httpClient.execute(request)){
+                return response.getEntity().getContent().readAllBytes();
+            }
         } catch (ClientProtocolException e) {
             throw new ApiExecutionException(e.getMessage(), e);
         } catch (URISyntaxException | IOException e) {
--- Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/CatFactsApplication.java
+++ Homework/java-core/06-Unit-Test/src/main/java/org/geekhub/hw6/CatFactsApplication.java
@@ -1,6 +1,7 @@
 package org.geekhub.hw6;
 
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.nio.file.Paths;
@@ -15,7 +16,12 @@ public class CatFactsApplication {
             String exceptionMessage = "The app hasn't received any arguments! Try to add one to program arguments!";
             throw new IllegalArgumentException(exceptionMessage);
         }
-        HttpClient httpClient = HttpClientBuilder.create().build();
+        RequestConfig config = RequestConfig.custom()
+            .setConnectTimeout(1500)
+            .setSocketTimeout(1500)
+            .setConnectionRequestTimeout(1500)
+            .build();
+        CloseableHttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
         CatFactsApiService apiService = new CatFactsApiService(httpClient);
         FileService fileService = new FileService(Paths.get(args[FILE_NAME_PROGRAM_ARGUMENT]));
         SchedulerService scheduler = new SchedulerService(apiService, fileService, Integer.parseInt(args[TIME_ARGUMENT]));
