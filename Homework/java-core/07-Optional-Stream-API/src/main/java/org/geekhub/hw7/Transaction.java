6fd2cc57bc9b28ae57aad148c2c3d4df0cd52861
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2023-12-02 12:13:04 +0200

    L07 Overridden equals(), hashcode() and toString() methods for Transaction class

--- Homework/java-core/07-Optional-Stream-API/src/main/java/org/geekhub/hw7/Transaction.java
+++ Homework/java-core/07-Optional-Stream-API/src/main/java/org/geekhub/hw7/Transaction.java
@@ -2,6 +2,27 @@ package org.geekhub.hw7;
 
 import javax.annotation.Nonnull;
 import java.time.LocalDate;
+import java.util.Objects;
 
 public record Transaction(double amount, @Nonnull String category, @Nonnull LocalDate date) {
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+
+        Transaction that = (Transaction) obj;
+        return Double.compare(this.amount, that.amount) == 0 &&
+            this.category.equals(that.category) &&
+            this.date.equals(that.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, category, date);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Transaction{amount=%.2f, category='%s', date=%s}", amount, category, date);
+    }
 }
