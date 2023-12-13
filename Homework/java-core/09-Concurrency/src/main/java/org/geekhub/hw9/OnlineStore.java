c4e3d31f0fed6486a5bbdc12686ac46f73d697f8
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2023-12-13 22:04:45 +0200

    L09 Implemented OnlineStore class

--- Homework/java-core/09-Concurrency/src/main/java/org/geekhub/hw9/OnlineStore.java
+++ Homework/java-core/09-Concurrency/src/main/java/org/geekhub/hw9/OnlineStore.java
@@ -2,10 +2,31 @@ package org.geekhub.hw9;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class OnlineStore {
 
-    private Map<String, Integer> positions = new ConcurrentHashMap<>();
+    private final ExecutorService saleService = Executors.newSingleThreadExecutor();
+    private final Map<String, Integer> positions = new ConcurrentHashMap<>();
+    private final AtomicInteger totalSales = new AtomicInteger(0);
+
+    public Future<Boolean> purchase(String positionName, int quantity) {
+        return saleService.submit(() -> {
+            if (positions.containsKey(positionName)) {
+                int productQuantity = positions.get(positionName);
+                int remaining = productQuantity - quantity;
+                if (remaining >= 0) {
+                    positions.replace(positionName, remaining);
+                    totalSales.getAndAdd(quantity);
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
 
     public void addProduct(String positionName, int amount) {
         positions.put(positionName, amount);
@@ -13,5 +34,9 @@ public class OnlineStore {
 
     public int getProductQuantity(String positionName) {
         return positions.get(positionName);
+    }
+
+    public int getTotalSales() {
+        return totalSales.get();
     }
 }
