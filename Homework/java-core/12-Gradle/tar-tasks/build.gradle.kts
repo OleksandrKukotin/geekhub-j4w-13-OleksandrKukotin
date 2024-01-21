aa1896e722b3e34495b5d622bf07454bc052a256
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-01-21 11:45:31 +0200

    L12: Implemented tar archiving task

--- Homework/java-core/12-Gradle/tar-tasks/build.gradle.kts
+++ Homework/java-core/12-Gradle/tar-tasks/build.gradle.kts
@@ -1,3 +1,21 @@
 plugins {
     id("java")
 }
+
+tasks.register<Tar>("archiveTask") {
+    archiveBaseName = "archive"
+    compression = Compression.GZIP
+
+    from(fileTree("src/main/resources").files) {
+        include("*.text")
+        includeEmptyDirs = false
+        rename { file ->
+            file.replaceFirst(".text", ".txt")
+        }
+    }
+
+    doLast {
+        logger.lifecycle("Files have been archived.")
+    }
+}
+
