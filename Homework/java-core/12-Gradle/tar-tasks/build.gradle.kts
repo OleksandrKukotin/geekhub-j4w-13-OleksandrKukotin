77697b4c1453f754a29e7ee242d8faf1b03b1137
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-02-06 22:39:30 +0200

    L12 refactor: moved archiving gradle script into a separate file applied in build.gradle.kts

--- Homework/java-core/12-Gradle/tar-tasks/build.gradle.kts
+++ Homework/java-core/12-Gradle/tar-tasks/build.gradle.kts
@@ -2,46 +2,4 @@ plugins {
     id("java")
 }
 
-tasks.register<Tar>("archiveTask") {
-    archiveBaseName = "archive"
-    compression = Compression.GZIP
-    destinationDirectory.set(fileTree("build/archive").dir)
-
-    from(fileTree("src/main/resources").files) {
-        include("*.text")
-        includeEmptyDirs = false
-        rename { file ->
-            file.replaceFirst(".text", ".txt")
-        }
-    }
-
-    doLast {
-        logger.lifecycle("Files have been archived.")
-    }
-}
-
-tasks.register("triggerArchive") {
-    dependsOn("archiveTask")
-}
-
-tasks.register<Copy>("unarchiveTask") {
-    dependsOn("triggerArchive")
-
-    from(tarTree("build/archive/archive.tgz").files) {
-        include("*.txt")
-        includeEmptyDirs = false
-    }
-    into(fileTree("src/test/resources").dir)
-
-    doLast {
-        logger.lifecycle("Files have been unarchived.")
-    }
-}
-
-tasks.register<Delete>("cleanup") {
-    doLast {
-        fileTree("build/archive").files.map { file -> file.delete() }
-        fileTree("src/test/resources").files.map { file -> file.delete() }
-        logger.lifecycle("Cleanup completed.")
-    }
-}
+apply(from = "tar-compression.gradle.kts")
