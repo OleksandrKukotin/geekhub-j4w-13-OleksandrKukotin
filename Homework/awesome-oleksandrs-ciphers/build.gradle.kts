a077f3d53527188a5f9b15295be29855624c662f
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-02-10 23:06:00 +0200

    L15: configured publishing logic

---
+++ Homework/awesome-oleksandrs-ciphers/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("java")
+    id("maven-publish")
+}
+
+group = "com.geekhub.homeworks"
+version = "0.0.1"
+
+publishing {
+    publications {
+        create<MavenPublication>("initialMavenPublication") {
+            from(components["java"])
+            pom {
+                name = "Awesome Oleksandr Ciphers"
+                description = "Java library that contains some classic ciphers"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "oleksandr_k"
+                        name = "Oleksandr Kukotin"
+                        email = "oleksandr.kukotin@gmail.com"
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://repsy.io/mvn/vrudas/okukotin-j4w-s13-repo")
+            credentials {
+                username = System.getenv("MAVEN_LOGIN")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+}
