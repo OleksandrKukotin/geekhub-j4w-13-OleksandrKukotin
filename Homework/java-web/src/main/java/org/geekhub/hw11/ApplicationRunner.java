ea907eda17322a31c30bdf0675fad268c7be67d8
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-02-17 18:00:38 +0200

    L16: changed building options

--- Homework/java-web/src/main/java/org/geekhub/hw11/ApplicationRunner.java
+++ Homework/java-web/src/main/java/org/geekhub/hw11/ApplicationRunner.java
@@ -2,8 +2,15 @@ package org.geekhub.hw11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    SecurityAutoConfiguration.class,
+    DataSourceAutoConfiguration.class,
+    FlywayAutoConfiguration.class
+})
 public class ApplicationRunner {
 
     public static void main(String[] args) {
