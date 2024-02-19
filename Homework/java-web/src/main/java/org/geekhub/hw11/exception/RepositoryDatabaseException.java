b898d959bef9764bc4eac17a94818f48d2cdd72c
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-02-19 19:58:28 +0200

    L16 refactoring: got rid of useless exception

--- Homework/java-web/src/main/java/org/geekhub/hw11/exception/RepositoryDatabaseException.java
+++
@@ -1,8 +0,0 @@
-package org.geekhub.hw11.exception;
-
-public class RepositoryDatabaseException extends RuntimeException {
-
-    public RepositoryDatabaseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
