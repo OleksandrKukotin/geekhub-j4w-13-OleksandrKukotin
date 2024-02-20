2a5eba646ac71a4c9c15f08ed20f1c29290e5c70
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-02-20 18:58:37 +0200

    L16 refactoring: got rid of Hikari datasource

--- Homework/java-web/src/main/java/org/geekhub/hw11/ApplicationRunner.java
+++ Homework/java-web/src/main/java/org/geekhub/hw11/ApplicationRunner.java
@@ -2,15 +2,8 @@ package org.geekhub.hw11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-    SecurityAutoConfiguration.class,
-    DataSourceAutoConfiguration.class,
-    FlywayAutoConfiguration.class
-})
+@SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
--- Homework/java-web/src/main/java/org/geekhub/hw11/config/DatasourceConfig.java
+++ Homework/java-web/src/main/java/org/geekhub/hw11/config/DatasourceConfig.java
@@ -1,9 +1,5 @@
 package org.geekhub.hw11.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -12,25 +8,6 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatasourceConfig {
-
-    @Bean
-    public DataSource dataSource(@Value("${spring.datasource.url}") String jdbcUrl,
-                                 @Value("${spring.datasource.username}") String user,
-                                 @Value("${spring.datasource.password}") String password) {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(jdbcUrl);
-        config.setUsername(user);
-        config.setPassword(password);
-        return new HikariDataSource(config);
-    }
-
-    @Bean(initMethod = "migrate")
-    public Flyway flyway(DataSource dataSource) {
-        return Flyway.configure()
-            .dataSource(dataSource)
-            .locations("db/migrations")
-            .load();
-    }
 
     @Bean
     NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
