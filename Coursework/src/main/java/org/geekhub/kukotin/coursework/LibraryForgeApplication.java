4b736c79ccbfb7d007eedf064af9d9493a0f39a8
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-02-24 09:32:16 +0200

    Coursework: configured database

--- Coursework/src/main/java/org/geekhub/kukotin/coursework/LibraryForgeApplication.java
+++ Coursework/src/main/java/org/geekhub/kukotin/coursework/LibraryForgeApplication.java
@@ -2,15 +2,8 @@ package org.geekhub.kukotin.coursework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-    SecurityAutoConfiguration.class,
-    DataSourceAutoConfiguration.class,
-    FlywayAutoConfiguration.class
-})
+@SpringBootApplication
 public class LibraryForgeApplication {
 
     public static void main(String[] args) {
