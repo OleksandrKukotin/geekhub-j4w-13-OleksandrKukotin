590dc733a31e79ef239527ea41bdc6c4790c02ce
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-02-28 10:53:46 +0200

    Java-web refactor: added -default configuration

--- Homework/java-web/build.gradle.kts
+++ Homework/java-web/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jdbc:3.2.2")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.2.2")
 
-    implementation("org.postgresql:postgresql:42.7.1")
+    implementation("org.postgresql:postgresql:42.7.2")
     implementation("org.flywaydb:flyway-core:10.7.1")
     runtimeOnly("org.flywaydb:flyway-database-postgresql:10.7.1")
 
