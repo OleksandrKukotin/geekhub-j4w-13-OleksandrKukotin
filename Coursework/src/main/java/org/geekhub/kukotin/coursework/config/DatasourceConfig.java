7c9e88cf036043d87d40f35f52fe1d2542803cb3
Oleksandr Kukotin <oleksandr.kukotin@gmail.com>
2024-03-20 16:46:22 +0200

    Coursework: configured docker-compose

---
+++ Coursework/src/main/java/org/geekhub/kukotin/coursework/config/DatasourceConfig.java
@@ -0,0 +1,16 @@
+package org.geekhub.kukotin.coursework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Bean
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+}
